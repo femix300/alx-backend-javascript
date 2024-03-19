4e5e78cbe17610da278f9eb69b4430a204db830f
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2024-03-19 20:01:13 +0100

    modifications to task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
+  return promise.then(() => {
     console.log('Got a response from the API');
     return { status: 200, body: 'success' };
-  }).catch(() => new Error());
+  }).catch(() => {
+    return new Error();
+  });
 }
