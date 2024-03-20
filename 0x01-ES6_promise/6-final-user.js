cc0795b6c7ed31c51f6222c7e0d150294593d87c
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2024-03-20 15:33:33 +0100

    added task 6 and made some mods to earlier tasks

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,16 +1,14 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then(([userResult, photoResult]) => [
-      {
-        status: userResult instanceof Error ? 'rejected' : 'fufilled',
-        value: userResult instanceof Error ? userResult.message : userResult,
-      },
-      {
-        status: photoResult instanceof Error ? 'rejected' : 'fufilled',
-        value: photoResult instanceof Error ? photoResult.message : photoResult,
-      },
-    ]);
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const [userResult, photoResult] = await Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]);
+
+  return [
+    { status: userResult.status, value: userResult.status === 'fulfilled' ? userResult.value : String(userResult.reason) },
+    { status: photoResult.status, value: photoResult.status === 'fulfilled' ? photoResult.value : String(photoResult.reason) },
+  ];
 }
