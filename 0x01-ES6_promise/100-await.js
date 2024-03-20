75178c8f3d36f090b8f0f11e55593565a122a1a1
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2024-03-20 16:40:09 +0100

    made some minor mods and readded task 100

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -6,9 +6,9 @@ export default async function asyncUploadUser() {
   try {
     const photo = await uploadPhoto();
     const user = await createUser();
-    res = { photo, user };
+    result = { photo, user };
   } catch (err) {
-    res = { photo: null, user: null };
+    result = { photo: null, user: null };
   }
-  return res;
+  return result;
 }
