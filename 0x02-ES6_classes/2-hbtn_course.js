954ad9baeb48d208d36f00010e4e9afdf3b8eed6
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2024-03-21 21:05:18 +0100

    different mod to task

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,8 +1,8 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   get name() {
@@ -32,13 +32,15 @@ export default class HolbertonCourse {
   }
 
   set students(value) {
-    if (!(value instanceof Array)) {
+    if (!Array.isArray(value)) {
       throw new TypeError('Students must be an array of strings');
     }
 
-    if (!value.every((student) => typeof student === 'string')) {
-      throw new TypeError('Students must be an array of strings');
+    for (const student of value) {
+      if (typeof student !== 'string') {
+        throw new TypeError('Students must be an array of strings');
+      }
+      this._students = value;
     }
-    this._students = value;
   }
 }
