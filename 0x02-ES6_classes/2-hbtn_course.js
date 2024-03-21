533cc778a58ada2816d8da07293906c58db134f7
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2024-03-21 20:46:25 +0100

    placed the code in the setter block

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -40,8 +40,7 @@ export default class HolbertonCourse {
       if (typeof student !== 'string') {
         throw new TypeError('Students must be an array of strings');
       }
+      this._students = value;
     }
-
-    this._students = value;
   }
 }
