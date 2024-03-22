8fa9b0537f328149cf56f4448d5806ffd89056ab
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2024-03-22 10:36:03 +0100

    added task four

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,38 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currInstance) {
+    if (!(currInstance instanceof Currency)) {
+      throw new TypeError('Parameter must be an instance of Currency');
+    }
+    this._currency = currInstance;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
