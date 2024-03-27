85d10b5a65426b9443d86638b64cd47280df1924
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2024-03-27 19:37:07 +0100

    added task three

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,7 @@
+export default function getStudentIdsSum(getListStudents) {
+  if (!Array.isArray(getListStudents)) {
+    return [];
+  }
+
+  return getListStudents.reduce((sum, student) => sum + student.id, 0);
+}
