de04fd9a268b7334805625955ef9f977185b9ea7
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2024-03-27 19:06:01 +0100

    added task two

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,7 @@
+export default function getStudentsByLocation(getListStudents, city) {
+  if (!Array.isArray(getListStudents)) {
+    return [];
+  }
+
+  return getListStudents.filter((student) => student.location === city);
+}
