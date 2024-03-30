40e2579d068ae8e9beea9857a8b824296fb74d03
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2024-03-30 01:03:58 +0100

    checked for empty sets and empty strings in task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,9 @@
 export default function cleanSet(set, startString) {
-  if (startString !== '') {
-    return Array.from(set)
-      .filter((element) => element.startsWith(startString))
-      .map((element) => element.substring(startString.length))
-      .join('-');
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
   }
-
-  return '';
+  return Array.from(set)
+    .filter((element) => element.startsWith(startString))
+    .map((element) => element.substring(startString.length))
+    .join('-');
 }
