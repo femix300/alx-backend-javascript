84c9b0dcb9b9cfbe2aba9d621766443c267c63c2
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2024-08-21 04:07:59 +0100

    added task 1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,12 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Welcome to Holberton School, what is your name?\n', (input) => {
+  const trimmedInput = input.trim();
+  console.log(`Your name is: ${trimmedInput}`);
+  rl.close();
+});
