17118691dd3337761de2901e29f843455566b5d7
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2024-08-24 15:13:56 +0100

    added task 3

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,40 @@
+const fs = require('fs').promises;
+
+/**
+ * This program reads a csv file and counts the number of students in it
+ * @param {string} path: Path to the csv file.
+ */
+
+const countStudents = async (path) => {
+  try {
+    const data = await fs.readFile(path, 'utf-8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+    if (lines.length === 0) {
+      throw new Error('Cannot load the database');
+    }
+
+    const fields = {};
+    let totalStudents = 0;
+
+    for (let i = 1; i < lines.length; i += 1) {
+      const [firstname, , , field] = lines[i].split(',');
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(firstname);
+      totalStudents += 1;
+    }
+
+    console.log(`Number of students: ${totalStudents}`);
+
+    for (const field in fields) {
+      if (Object.prototype.hasOwnProperty.call(fields, field)) {
+        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+      }
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+};
+module.exports = countStudents;
