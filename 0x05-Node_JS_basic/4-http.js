7d3aff7cd662478fcab6ad1c320211f96bf0e93f
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2024-08-25 18:28:25 +0100

    modified the code in task 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,6 +3,8 @@ const http = require('http');
 const port = 1245;
 
 const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
